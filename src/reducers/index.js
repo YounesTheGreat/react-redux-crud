654f1fcf5e067fa058722c7e7f607ae3c7cb33fd
younes <youneskasri@gmail.com>
2019-08-20 20:16:50 +0100

    defining user reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,15 +1,15 @@
-export const userReducer = (state, action) => {
+export const userReducer = (state = [], action) => {
     switch (action.type) {
-        case "ADD_USER": 
-            break;
+        case "ADD_USER":
+            return [...state, action.payload]; 
         case "REMOVE_USER":
-            break;
+            return state.filter(user => user.name !== action.payload.name);
         default:
             return state;
     }
 };
 
-export const secondReducer = (state, action) => {
+export const secondReducer = (state = {}, action) => {
     return state;
 };
 
--- src/store.js
+++ src/store.js
@@ -1,6 +1,18 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import { userReducer, secondReducer } from "./reducers";
 
-const store = createStore(combineReducers({ userReducer, secondReducer }));
+const logger = (state) => (next) => (action) => {
+    console.log("Action : ", action);
+    next(action);
+};
+
+const store = createStore(
+    combineReducers({ users: userReducer, others: secondReducer }),    
+    {},
+    applyMiddleware(logger));
 
 export default store;
+
+store.dispatch({ type: "ADD_USER", payload: { name: "Younes "}});
+
+console.log(store.getState());
